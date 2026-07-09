3392d4443e17b8ba51554934f94c801815c99414
derek <derekantuanj@gmail.com>
2026-07-08 19:03:00 -0600

    cambios

--- backend/controllers/maestro.controller.js
+++ backend/controllers/maestro.controller.js
@@ -4,13 +4,13 @@ import {successResponse, errorResponse} from '../utils/helpers.util.js';
 
 export const obtenerTodos = async (req, res) => {
     try {
-        const maestroDarta = req.body;
+        const maestroData = req.body;
         const maestros = await obtenerTodosMaestros();
         successResponse(res, 201, 'Maestros obtenidos exitosamente', maestros);
 
     }catch (error) {
         console.error('Error al obtener maestros:', error);
-        errorResponse(res, 500, 'Error al obtener maestros');
+        errorResponse(res, 200, 'Error al obtener maestros');
     }
 };
 
